2ca099ccd91f18b0c035b2dfbf6a65f683887612
wagdynavas <wnavas@fispan.com>
2025-04-11 16:31:50 -0400

    feat(WN-initial-setup): First commit with project structure.

---
+++ src/main/java/com/wagdynavas/selfkeep/SelfkeepApplication.java
@@ -0,0 +1,13 @@
+package com.wagdynavas.selfkeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfkeepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfkeepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wagdynavas/selfkeep/SelfkeepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wagdynavas.selfkeep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfkeepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
