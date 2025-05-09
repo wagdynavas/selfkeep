4fe3937ffb47de917d407eae773dde2b11e4bc0e
wagdynavas <wnavas@fispan.com>
2025-05-08 21:36:38 -0400

    feat(WN-auth): Update User Repository and error message

--- src/main/java/com/wagdynavas/selfkeep/UserErrors.java
+++ src/main/java/com/wagdynavas/selfkeep/UserErrors.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 public enum UserErrors {
 
     UNMATCHED_USER_PASSWORD("UUP-400", "Password and confirmPassword does not match."),
-    NOT_UNIQUE_USER_EMAIL("NUE-400", "Email already has an account registered."),
+    NOT_UNIQUE_USER_EMAIL("NUE-400", "Email/Username already has an account registered."),
     EMAIL_NOT_FOUND("ENF-400", "Unable to find user with provided email.");
 
     private final String errorCode;
--- src/main/java/com/wagdynavas/selfkeep/repositoty/UserRepository.java
+++ src/main/java/com/wagdynavas/selfkeep/repositoty/UserRepository.java
@@ -10,4 +10,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
 
     Optional<User> findByEmail(String email);
+
+    Optional<User> findByEmailOrUsername(String email, String username);
 }
